f33907d7e99ca08be7cf1152e511f73774d6894f
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2021-09-19 17:12:33 +0900

    コメント追加

--- src/main/java/blog/tsuchiya/tutorial/step1/controller/OutputController.java
+++ src/main/java/blog/tsuchiya/tutorial/step1/controller/OutputController.java
@@ -10,10 +10,14 @@ import blog.tsuchiya.tutorial.step1.controller.form.InputForm;
 @Controller
 public class OutputController {
 
-	// postメソッドのときに呼ばれることを宣言
-	// getでこのURLが指定されたら405エラー
+	// postメソッドのときに呼ばれることを宣言。
+	// getでこのURLが指定されたら405エラー。
 	@PostMapping("output")
+	// 2つの方法で入力Formから値を取得している。
+	// @RequestParamを使うと、特定の1つの値を取得できる。
+	// クラスを使うとまとめて複数の値を取得できる。
 	public String output(@RequestParam(required = false) String text1, InputForm inputForm, Model model) {
+		// Modelに値を格納してThymeleafに渡す。
 		model.addAttribute("output1", text1);
 		model.addAttribute("output2", inputForm);
 		return "output";
